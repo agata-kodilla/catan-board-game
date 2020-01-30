f5f4165fe69b71eaa6f6d2a2ef4b0bd41465e84d
abialkowska <abialkowska247@gmail.com>
2020-01-30 19:18:14 +0100

    add roll dice method

--- src/main/java/Banker.java
+++ src/main/java/Banker.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class Banker {
     private int allCards;
     private int availableCard;
     private int userScore;
     private int playerAmount;
+    private int rollDice;
+
+    public Banker() {
+        allCards = 95;
+    }
+
+    public Integer rollDice() {
+        Random random = new Random();
+        return  random.nextInt(6) + 1;
+    }
 }
