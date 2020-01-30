ae6039da8afc517689406119b537ee92260cd18d
abialkowska <abialkowska247@gmail.com>
2020-01-30 19:17:38 +0100

    create build methods
    road, city, settlement, selfdevelopment

--- src/main/java/BuildItem.java
+++ src/main/java/BuildItem.java
@@ -1,16 +1,38 @@
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class BuildItem {
 
-    public void buildRoad(List<Card> cards) {
+    public void buildRoad(User user) {
 
+        if (user.getSources().get(SourceType.BRICK) >= 1 && user.getSources().get(SourceType.WOOD) >= 1) {
+            user.getSources().put(SourceType.BRICK, user.getSources().get(SourceType.BRICK) - 1);
+            user.getSources().put(SourceType.WOOD, user.getSources().get(SourceType.WOOD) - 1);
+        }
     }
 
-    public void buildCity(List<Card> cards) {
-
+    public void buildCity(User user) {
+        if (user.getSources().get(SourceType.WHEAT) >= 2 && user.getSources().get(SourceType.ORE) >= 3) {
+            user.getSources().put(SourceType.WHEAT, user.getSources().get(SourceType.WHEAT) - 2);
+            user.getSources().put(SourceType.ORE, user.getSources().get(SourceType.ORE) - 3);
+        }
     }
 
-    public void buildSettlement(List<Card> cards) {
+    public void buildSettlement(User user) {
+        if (user.getSources().get(SourceType.BRICK) >= 1 && user.getSources().get(SourceType.WOOD) >= 1 && user.getSources().get(SourceType.WHEAT) >= 1 && user.getSources().get(SourceType.SHEEP) >= 1) {
+            user.getSources().put(SourceType.BRICK, user.getSources().get(SourceType.BRICK) - 1);
+            user.getSources().put(SourceType.WOOD, user.getSources().get(SourceType.WOOD) - 1);
+            user.getSources().put(SourceType.SHEEP, user.getSources().get(SourceType.SHEEP) - 1);
+            user.getSources().put(SourceType.WHEAT, user.getSources().get(SourceType.WHEAT) - 1);
+        }
+    }
 
+    public void buildSelfdevelopment(User user) {
+        if (user.getSources().get(SourceType.WHEAT) >= 1 && user.getSources().get(SourceType.SHEEP) >= 1 && user.getSources().get(SourceType.ORE) >= 1) {
+            user.getSources().put(SourceType.SHEEP, user.getSources().get(SourceType.SHEEP) - 1);
+            user.getSources().put(SourceType.ORE, user.getSources().get(SourceType.ORE) - 1);
+            user.getSources().put(SourceType.WHEAT, user.getSources().get(SourceType.WHEAT) - 1);
+        }
     }
 }
