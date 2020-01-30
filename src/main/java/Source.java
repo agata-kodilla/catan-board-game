9749fcc27918fd8b40995303764522a26c658598
abialkowska <abialkowska247@gmail.com>
2020-01-30 19:16:41 +0100

    add source class

---
+++ src/main/java/Source.java
@@ -0,0 +1,47 @@
+public class Source {
+    int brickAmount;
+    int woodAmount;
+    int wheatAmount;
+    int sheepAmount;
+    int oreAmount;
+
+    public int getBrickAmount() {
+        return brickAmount;
+    }
+
+    public void setBrickAmount(int brickAmount) {
+        this.brickAmount = brickAmount;
+    }
+
+    public int getWoodAmount() {
+        return woodAmount;
+    }
+
+    public void setWoodAmount(int woodAmount) {
+        this.woodAmount = woodAmount;
+    }
+
+    public int getWheatAmount() {
+        return wheatAmount;
+    }
+
+    public void setWheatAmount(int wheatAmount) {
+        this.wheatAmount = wheatAmount;
+    }
+
+    public int getSheepAmount() {
+        return sheepAmount;
+    }
+
+    public void setSheepAmount(int sheepAmount) {
+        this.sheepAmount = sheepAmount;
+    }
+
+    public int getOreAmount() {
+        return oreAmount;
+    }
+
+    public void setOreAmount(int oreAmount) {
+        this.oreAmount = oreAmount;
+    }
+}
---
+++ src/main/java/SourceType.java
@@ -0,0 +1,3 @@
+public enum SourceType {
+    BRICK, ORE, SHEEP, WHEAT, WOOD;
+}
